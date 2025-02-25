abc363b9080e4fc5e2b5fbbed97ec46ed2b1d5b1
Kai <me@kai.enterprises>
2025-02-25 15:44:50 +0100

    Revert "feat: use type & file name from headers if available"
    This reverts commit 25b14831945c536a9e3fae453cbddbe338f77617.

--- src/index.ts
+++ src/index.ts
@@ -61,27 +61,12 @@ const fileTypesMap: Record<string, string> = {
 };
 
 const files = await Promise.all(filePaths.map(async filePath => {
-    let type = fileTypesMap[path.extname(filePath)];
-
+    const type = fileTypesMap[path.extname(filePath)];
     // Check if the path is an url
     if (/^https?:\/\//.test(filePath)) {
         const res = await fetch(filePath);
         const data = await res.arrayBuffer();
-        // If available, get a type from the Content-Type header
-        if (res.headers.has('Content-Type')) {
-            type = res.headers.get('Content-Type') || type;
-        }
-
-        // Try to get a file name from the Content-Disposition header
-        let filename = path.basename(filePath)
-        if (res.headers.has("Content-Disposition")) {
-            const header = res.headers.get("Content-Disposition")!;
-            const matches = /filename\*=UTF-8''([^;\n]+)|filename="?([^"\n;]+)"?/i.exec(header);
-            if (matches) {
-                filename = (matches[1] || matches[2]) ?? filename;
-            }
-        }
-        return new File([data], filename, {type});
+        return new File([data], path.basename(filePath), {type});
     }
 
     const data = await fs.readFile(filePath);
